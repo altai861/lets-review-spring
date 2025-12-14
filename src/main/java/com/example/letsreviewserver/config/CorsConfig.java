188699b5f831da9a3fc8b3356742ce2754374a58
altai <bbyyydriver@gmail.com>
2025-12-14 10:03:33 +0800

    github CI workflow file oruulav

--- src/main/java/com/example/letsreviewserver/config/CorsConfig.java
+++ src/main/java/com/example/letsreviewserver/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**") // Apply this configuration to all paths starting with /api
-                .allowedOrigins("http://localhost:4200") // ⬅️ THIS IS THE CRITICAL PART
+                .allowedOrigins("http://localhost:4200", "https://demo.uneliy.org") // ⬅️ THIS IS THE CRITICAL PART
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
